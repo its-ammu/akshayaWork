3c39c4ddfb1c97d5b78a5fbe78c8f6723b896413
its-ammu <amuthavarsnirajkumar@gmail.com>
2025-01-05 18:57:07 +0530

    dashboard setup done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
+
+// @NgModule({ imports: [RouterModule.forRoot(routes)], exports: [RouterModule] })
+// export class AppRoutingModule { }
